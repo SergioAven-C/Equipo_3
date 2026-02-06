3ecd6673ff46bd7826714e065ee34030fcef10af
SergioAAC <al07092161@tecmilenio.mx>
2026-02-06 11:06:32 -0600

    Update firebase.js

--- Actividad 2/firebase.js
+++ Actividad 2/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyCOKIdvGOVjACaZfvs8PJkTmkYG9b43Buk",
+  authDomain: "veterinaria-1496e.firebaseapp.com",
+  projectId: "veterinaria-1496e",
+  storageBucket: "veterinaria-1496e.firebasestorage.app",
+  messagingSenderId: "225229833685",
+  appId: "1:225229833685:web:bd4791ba2e7d49d015695c"
 };
 
 const app = initializeApp(firebaseConfig);
